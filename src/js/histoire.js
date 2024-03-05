906d298ff1de966bda1bacf2474c025a24ff48af
lucile berger <regrebl9@gmail.com>
2024-03-05 18:26:15 +0100

    page d'intro

--- src/js/histoire.js
+++ src/js/histoire.js
@@ -23,7 +23,7 @@ export default class histoire extends Phaser.Scene {
   
     preload() {
   
-        this.load.image("histoire", "./src/assets/hist.PNG");
+        this.load.image("histoire", "./src/assets/parch.jpg");
   
       this.load.image("imageBoutonPlay", "./src/assets/Bouton_Play1.png");
   
@@ -45,11 +45,11 @@ export default class histoire extends Phaser.Scene {
   
       //on ajoute un bouton de clic, nommé bouton_play
   
-      var bouton_play = this.add.image(500, 600, "imageBoutonPlay").setDepth(1).setDisplaySize(90, 90);
+      var bouton_play = this.add.image(400, 800, "imageBoutonPlay").setDepth(1).setDisplaySize(80, 80);
   
       // Texte à ajouter en blanc gris avec la police Chiller en gras
   
-      var texte = this.add.text(60, 100, "Au cœur d'une forêt dense se dresse un manoir abandonné. \n\nSelon la légende, ce manoir renferme d'innombrables secrets et énigmes anciennes, \ngardées par un esprit mystérieux. \nLes rumeurs disent que quiconque parvient à résoudre toutes les énigmes du manoir \net par conséquent libérer l’esprit du château sera récompensé par un trésor légendaire. \nAttention, si vous échouez, vous y serez piégé pour l'éternité...\n\nVous incarnez un aventurier courageux qui décide de relever le défi \net de percer les mystères du manoir.", { font: "bold 35px Chiller", fill: "#c0c0c0" }).setDepth(1);
+      var texte = this.add.text(240, 150, "Au cœur d'une forêt dense se dresse un manoir abandonné. \n\nSelon la légende, ce manoir renferme d'innombrables secrets \net énigmes anciennes, gardées par un esprit mystérieux. \nLes rumeurs disent que quiconque parvient à résoudre \ntoutes les énigmes du manoir et par conséquent \n libérer l’esprit du château sera récompensé \npar un trésor légendaire. \nAttention, si vous échouez, vous y serez piégé pour l'éternité...\n\nVous incarnez un aventurier courageux qui décide de relever\n le défi et de percer les mystères du manoir.", { font: "bold 26px Papyrus", fill: "#EDE9CE" }).setDepth(1);
   
       //=========================================================
   
