b088efb86539d52dbf5ca6790553eb8d7c4a7c92
PaulineCantie <cantie.pauline@gmail.com>
2024-03-05 18:27:06 +0100

    Niveau 4

--- src/js/niveau4.js
+++ src/js/niveau4.js
@@ -2,48 +2,52 @@ export default class niveau4 extends Phaser.Scene {
   // constructeur de la classe
   constructor() {
     super({
-      key: "niveau4" //  ici on précise le nom de la classe en tant qu'identifiant
+      key: "niveau4",
+      physics: {
+        // définition des parametres physiques
+        default: "arcade", // mode arcade : le plus simple : des rectangles pour gérer les collisions. Pas de pentes
+        arcade: {
+          // parametres du mode arcade
+          gravity: {
+            y: 300 // gravité verticale : acceleration ddes corps en pixels par seconde
+          },
+          debug: false // permet de voir les hitbox et les vecteurs d'acceleration quand mis à true
+        }
+      } //  ici on précise le nom de la classe en tant qu'identifiant
     });
   }// chargement des librairies
 
-/***********************************************************************/
-/** CONFIGURATION GLOBALE DU JEU ET LANCEMENT 
-/***********************************************************************/
+  preload() {
+    this.load.image("img_ciel", "src/assets/fondenigmeattraperindice.png");
+    this.load.image("img_plateforme", "src/assets/plateformeOk.jpg");
+    this.load.image("img_etoile", "src/assets/bouledepapierokk.png"); 
+    this.load.image("img_bombe", "src/assets/BouledePapier4.png"); 
+    this.load.image("img_plateforme2", "src/assets/platformeok.png");
+    this.load.image("img_reussite", "src/assets/Parchemin.png"); 
+ 
 
-preload() {
-  this.load.image("img_ciel", "src/assets/fondenigmeattraperindice.png");
-  this.load.image("img_plateforme", "./src/assets/platform.png");
-  this.load.image("img_etoile", "src/assets/BouledePapier2.jpg"); 
-  this.load.image("img_bombe", "src/assets/couteau.jpg"); 
+  
+    this.load.spritesheet("img_perso", "src/assets/dude.png", {
+      frameWidth: 32,
+      frameHeight: 48
+    }); 
+  
+    this.load.image("img_etoile", "src/assets/bouledepapier.jpg");
+} 
 
-  this.load.spritesheet("img_perso", "src/assets/dude.png", {
-    frameWidth: 32,
-    frameHeight: 48
-  }); 
-
-  this.load.image("img_etoile", "src/assets/star.png"); 
-}
-
-
-/***********************************************************************/
-/** FONCTION CREATE 
-/***********************************************************************/
-
-/* La fonction create est appelée lors du lancement de la scene
- * si on relance la scene, elle sera appelée a nouveau
- * on y trouve toutes les instructions permettant de créer la scene
- * placement des peronnages, des sprites, des platesformes, création des animations
- * ainsi que toutes les instructions permettant de planifier des evenements
- */
 create() {
-  this.add.image(500, 500, "img_ciel");
+  this.add.image(500, 600, "img_ciel");
   groupe_plateformes = this.physics.add.staticGroup();
+  groupe_plateformes.create(200, 1400, "img_plateforme");
+  groupe_plateformes.create(50, 644, "img_plateforme2");
+  groupe_plateformes.create(1000, 784, "img_plateforme2");
+
   
   this.load.spritesheet("img_perso", "src/assets/dude.png", {
     frameWidth: 32,
     frameHeight: 48,
   });
-  player = this.physics.add.sprite(100,800, "img_perso");
+  player = this.physics.add.sprite(100, 450, "img_perso");
   player.setCollideWorldBounds(true);
   this.physics.add.collider(player, groupe_plateformes);
   player.setBounce(0.2); 
@@ -68,32 +72,36 @@ create() {
     frames: [{ key: "img_perso", frame: 4 }],
     frameRate: 20
   }); 
-  
+  this.physics.add.collider(player, groupe_plateformes); 
+
   //  On rajoute un groupe d'étoiles, vide pour l'instant
   groupe_etoiles = this.physics.add.group(); 
   // on rajoute 10 étoiles avec une boucle for :
   // on répartit les ajouts d'étoiles tous les 70 pixels sur l'axe des x
   for (var i = 0; i < 10; i++) {
-    var coordX = 70 + 70 * i;
+    var coordX = 85 + 85 * i;
     groupe_etoiles.create(coordX, 10, "img_etoile");
   } 
   this.physics.add.collider(groupe_etoiles, groupe_plateformes); 
   groupe_etoiles.children.iterate(function iterateur(etoile_i) {
     // On tire un coefficient aléatoire de rerebond : valeur entre 0.4 et 0.8
-    var coef_rebond = Phaser.Math.FloatBetween(0.4, 0.8);
+    var coef_rebond = Phaser.Math.FloatBetween(0.3, 0.7);
     etoile_i.setBounceY(coef_rebond); // on attribut le coefficient de rebond à l'étoile etoile_i
   }); 
   this.physics.add.overlap(player, groupe_etoiles, ramasserEtoile, null, this);
 
-  zone_texte_score = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' }); 
+  zone_texte_score = this.add.text(50, 170, 'score: 0', { fontSize: '64px', fill: '#fff', fontFamily: 'Arial, sans-serif'}); 
 
  groupe_bombes = this.physics.add.group(); 
  this.physics.add.collider(groupe_bombes, groupe_plateformes); 
  this.physics.add.collider(player, groupe_bombes, chocAvecBombe, null, this); 
-}
-/***********************************************************************/
-/** FONCTION UPDATE 
-/***********************************************************************/
+
+ 
+
+ 
+} 
+
+
 update() {
   if (clavier.right.isDown) {
     player.setVelocityX(160);
@@ -109,23 +117,14 @@ update() {
   } 
   if (clavier.space.isDown && player.body.touching.down) {
     player.setVelocityY(-330);
+
     if (gameOver) {
       return;
-    } 
-} }
+    }} 
+    
+
 }
-
-
-
-
-var player; // désigne le sprite du joueur
-var clavier; 
-var groupe_plateformes;
-var groupe_etoiles;
-var score = 0;
-var zone_texte_score; 
-var groupe_bombes; 
-var gameOver = false;
+}
 
 function ramasserEtoile(un_player, une_etoile) {
   // on désactive le "corps physique" de l'étoile mais aussi sa texture
@@ -165,3 +164,12 @@ if (groupe_etoiles.countActive(true) === 0) {
     player.anims.play("anim_face");
     gameOver = true;
   } 
+
+var groupe_plateformes; 
+var player;
+var clavier; 
+var groupe_etoiles;
+var score = 0;
+var zone_texte_score; 
+var groupe_bombes; 
+var gameOver = false;
